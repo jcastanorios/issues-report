d21df8a6fb266484498e5fe65d7dea1ed68b10bc
Juan Gonzalo Castano Rios <juan.castano@mercadolibre.com.co>
2024-05-04 00:02:57 -0500

    se implementa prueba automatizada para adicionar un miembro a un blog

--- cypress/e2e/members.cy.js
+++ cypress/e2e/members.cy.js
@@ -1,99 +1,67 @@
 import LoginGhost from "../support/login";
+import MemberObjectModel from "../support/memberObjectModel";
+import { faker } from "@faker-js/faker";
+import { Constantes } from "../support/constantes";
 
-describe("Escenario para creación de un miembro de un blog", () => {
-  const USER_GHOST = "wilderlopezm@gmail.com";
-  const PASS_GHOST = "12345678901";
-  let totalMembers=0;
-
+describe("Escenario de pruebas para creación de un miembro para un blog", () => {
+  let totalMembers = 0;
   beforeEach(() => {
-    
+    LoginGhost.visit();
+    LoginGhost.diligenciarEmail(Constantes.USER_GHOST);
+    LoginGhost.diligenciarPassword(Constantes.PASS_GHOST);
+    LoginGhost.clickBotonSignIn();
   });
 
-  it("Primer caso diligenciar las credenciales correctas para poder acceder al dashboard de la aplicación", () => {
-    LoginGhost.visit();
-    LoginGhost.diligenciarEmail(USER_GHOST);
-    LoginGhost.diligenciarPassword(PASS_GHOST);
-    LoginGhost.clickBotonSignIn();
+  /**
+   * Precondiciones: un usuario logueado en el sistema
+   * Postcondiciones: un miembro creado en el sistema
+   */
+  it("Creación nuevo miembro del blog", () => {
+    //Given 
+    //verificar que navegamos hasta la pantalla de la lista de miembros
+    MemberObjectModel.clicOpcionMenuMiembros();
+    MemberObjectModel.validarAccesoPaginaMiembros().then((response) => {
+      expect(response.status).to.eq(200);
+    });
 
-    cy.request("https://ghost-aaej.onrender.com/ghost/#/dashboard").then(
-      (response) => {
-        expect(response.status).to.eq(200);
-      }
-    );
+    //verificar que si nos encontremos en la pantalla, revisar titulo de la sección
+    MemberObjectModel.validarTituloSeccionMiembros().then((text) => {
+      expect(text.trim()).to.eq(Constantes.TITULO_PAGINA_MIEMBROS);
+    });
+
+    //obtener el total de miembros en el blog
+    MemberObjectModel.obtenerTotalMiembrosBlog().then((text) => {
+      totalMembers = text;
+      console.log(Constantes.LBL_TOTAL_MIEMBROS, totalMembers);
+    });
+
+    //When
+    //Pulsar en botón para añadir nuevo miembro
+    MemberObjectModel.adicionarNuevoMiebro();
+
+    //nuevo nombre para un miembro
+    MemberObjectModel.adicionarNombreMiembro(faker.person.fullName());
+
+    //pulsar Save para adicionar un miembro sin email
+    MemberObjectModel.almacenarMiembroBlog();
+
+    //validar que el Save al detectar un error se convierta en un Retry
+    MemberObjectModel.obtenerBotonRetry().invoke("text").then((text) => {
+      expect(text.trim()).to.eq("Retry");
+    });
+    //nuevo nombre para un miembro
     cy.wait(2000);
-    cy.get('a[id="ember28"]').click();
-    cy.request("https://ghost-aaej.onrender.com/ghost/#/members").then(
-      (response) => {
-        expect(response.status).to.eq(200);
-      }
-    );
+    MemberObjectModel.adicionarEmailMiembro(faker.internet.email());
 
-    cy.get("h2.gh-canvas-title")
-      .invoke("text")
+    //pulsar btn Retry
+    MemberObjectModel.obtenerBotonRetry().click();
+
+    //Then
+    //validar que el miembro se haya añadido según total en la lista
+    cy.wait(2000);
+    MemberObjectModel.obtenerBaggedMiembros().invoke("text")
       .then((text) => {
-        cy.get("h2.gh-canvas-title")
-          .invoke("attr", "class")
-          .then((clase) => {
-            expect(text.trim()).to.eq("Members");
-          });
-      });
-
-    cy.wait(1000);
-    cy.get("div.gh-nav-top")
-      .find("a.active.ember-view")
-      .find("span.gh-nav-member-count")
-      .invoke("text")
-      .then((text) => {
-        totalMembers=text
-        console.log('total miembros al inicio', totalMembers)
-      });
-
-    cy.get("div.view-actions-top-row")
-      .find("a.ember-view.gh-btn.gh-btn-primary")
-      .contains("span", "New member")
-      .click();
-
-    cy.get('input[id="member-name"]').blur().type("Juan Gonzalo Castaño");
-
-    cy.get("div.gh-canvas-header")
-      .find("header.gh-canvas-header-content")
-      .find("section.view-actions")
-      .find("button.gh-btn.gh-btn-primary.gh-btn-icon.ember-view")
-      .find("span")
-      .contains("Save")
-      .click();
-
-    cy.get("div.gh-canvas-header")
-      .find("header.gh-canvas-header-content")
-      .find("section.view-actions")
-      .find("button.gh-btn.gh-btn-primary.gh-btn-icon.ember-view")
-      .find("span")
-      .contains("Retry")
-      .invoke("text")
-      .then((text) => {
-        expect(text.trim()).to.eq("Retry");
-      });
-
-    cy.wait(3000);
-    cy.get("div.form-group.max-width.error.ember-view")
-      .find("input.ember-text-field.gh-input.ember-view")
-      .type("estudiante"+totalMembers+"@email.edu.co");
-
-    cy.get("div.gh-canvas-header")
-      .find("header.gh-canvas-header-content")
-      .find("section.view-actions")
-      .find("button.gh-btn.gh-btn-primary.gh-btn-icon.ember-view")
-      .find("span")
-      .contains("Retry")
-      .click();
-
-    cy.wait(3000);
-    cy.get("div.gh-nav-top")
-      .find("a.active.ember-view")
-      .find("span.gh-nav-member-count")
-      .invoke("text")
-      .then((text) => {
-        expect(Number(totalMembers)+1).to.eq(Number(text));
+        expect(Number(totalMembers) + 1).to.eq(Number(text));
       });
   });
 });
---
+++ cypress/support/constantes.js
@@ -0,0 +1,5 @@
+export class Constantes {
+  static USER_GHOST = "wilderlopezm@gmail.com";
+  static PASS_GHOST = "12345678901";
+  static TITULO_PAGINA_MIEMBROS = "Members";
+}

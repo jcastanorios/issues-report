5141a455a66d01b5f8bffa062822f8cc11cab723
wilderlopezm2024 <w.lopezm@uniandes.edu.co>
2024-05-05 19:59:06 -0500

    Correcciones duplicidad steps

--- kraken/features/web/step_definitions/wlm_escenario18_step.js
+++ kraken/features/web/step_definitions/wlm_escenario18_step.js
@@ -1,34 +1,7 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
-When('I enter email {kraken-string}', async function (email) {
-    let element = await this.driver.$('input[name=identification]');
-    return await element.setValue(email);
-});
-When('I enter password {kraken-string}', async function (password) {
-    let element = await this.driver.$('input[name=password]'); 
-    return await element.setValue(password);
-});
-
-When('I click login', async function() {
-    let element = await this.driver.$('button[type=submit]');
-    return await element.click();
-});
 
 When('I click lista post', async function() {
     let element = await this.driver.$('section > div > ul:nth-child(2) > li:nth-child(1)');
-    return await element.click();
-});
-
-Then('I click new post', async function () {
-    let element = await this.driver.$('a.ember-view.gh-btn.gh-btn-primary');
-    return await element.click();
-});
-Then('I enter post titulo {kraken-string}', async function (posTitulo) {
-    let element = await this.driver.$('textarea.gh-editor-title');
-    return await element.setValue(posTitulo);
-});
-
-Then('I click lateral', async function () {
-    let element = await this.driver.$('button.settings-menu-toggle.gh-btn.gh-btn-editor.gh-btn-icon.icon-only.gh-btn-action-icon');
     return await element.click();
 });
 
@@ -36,16 +9,7 @@ Then('I click post para cerrar', async function () {
     let element = await this.driver.$('a.ember-view.gh-btn-editor.gh-editor-back-button');
     return await element.click();
 });
-
-
 When('I click lista post borrador', async function() {
     let element = await this.driver.$('a[href="#/posts/?type=draft"]');
     return await element.click();
 });
-
-Then('I tester new post {kraken-string}', async function (expectedText) {
-    const assert = require('assert');
-    let bodyText = await this.driver.$('body').getText();
-    assert(bodyText.includes(expectedText), `Expected text "${expectedText}" not found in page`);
-});
-
--- kraken/features/web/step_definitions/wlm_escenario19_step.js
+++ kraken/features/web/step_definitions/wlm_escenario19_step.js
@@ -1,45 +1,6 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
-When('I enter email {kraken-string}', async function (email) {
-    let element = await this.driver.$('input[name=identification]');
-    return await element.setValue(email);
-});
-When('I enter password {kraken-string}', async function (password) {
-    let element = await this.driver.$('input[name=password]'); 
-    return await element.setValue(password);
-});
-
-When('I click login', async function() {
-    let element = await this.driver.$('button[type=submit]');
-    return await element.click();
-});
-
-When('I click lista page', async function() {
-    let element = await this.driver.$('a[href="#/pages/"]');
-    return await element.click();
-});
-
-Then('I click new page', async function () {
-    let element = await this.driver.$('a[href="#/editor/page/"]');
-    return await element.click();
-});
-Then('I enter page titulo {kraken-string}', async function (posTitulo) {
-    let element = await this.driver.$('textarea.gh-editor-title');
-    return await element.setValue(posTitulo);
-});
-
-Then('I click lateral', async function () {
-    let element = await this.driver.$('button.settings-menu-toggle.gh-btn.gh-btn-editor.gh-btn-icon.icon-only.gh-btn-action-icon');
-    return await element.click();
-});
 
 Then('I click page para cerrar', async function () {
     let element = await this.driver.$('a.ember-view.gh-btn-editor.gh-editor-back-button');
     return await element.click();
 });
-
-Then('I tester new page {kraken-string}', async function (expectedText) {
-    const assert = require('assert');
-    let bodyText = await this.driver.$('body').getText();
-    assert(bodyText.includes(expectedText), `Expected text "${expectedText}" not found in page`);
-});
-

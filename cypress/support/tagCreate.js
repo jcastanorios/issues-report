4fc7de80802942b41bfb7b56d83480677121ebdd
wilderlopezm2024 <w.lopezm@uniandes.edu.co>
2024-05-12 00:52:52 -0500

    Cypress ALL3-ESC-GHOST_v4

--- cypress/support/tagCreate.js
+++ cypress/support/tagCreate.js
@@ -1,5 +1,7 @@
 import { faker } from '@faker-js/faker'; // Importar faker para generar datos falsos
 import { Constantes } from "../support/constantes";
+import ScreenshotPage from "../support/screenshot"; // Importar módulo para capturar pantallas
+
 class TagCreate{
 
     visit() {
@@ -9,15 +11,17 @@ class TagCreate{
             cy.visit("https://ghost-t6x4.onrender.com/ghost/#/tags");
         }
     }
-    clickNewTag(){
+    clickNewTag(nombreEscenario){
         cy.get('a.ember-view.gh-btn.gh-btn-primary').click()  //click en New tag
+        ScreenshotPage.takeScreenshot(nombreEscenario, 'clickNewTag'); 
     }
-    CreateNewTag(tagTitulo){
+    CreateNewTag(tagTitulo, nombreEscenario){
         let descript = faker.lorem.paragraph(3); // Generar un título de post aleatorio
         cy.get('input#tag-name.gh-input').type(tagTitulo) //nombre
         cy.get('input[placeholder=15171A]').type('7a0000') //color rojo
         cy.get('textarea#tag-description.gh-input.gh-tag-details-textarea').type(descript) //description
         cy.get('button.gh-btn.gh-btn-primary.gh-btn-icon.ember-view').click()//save
+        ScreenshotPage.takeScreenshot(nombreEscenario, 'CreateNewTag'); 
     }
 }
 export default new TagCreate();

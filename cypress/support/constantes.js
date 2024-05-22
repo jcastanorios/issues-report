499e4b7790d897a42b97cb04381b323e1f8e9cdd
fquinteroc <f.quinteroc@uniandes.edu.co>
2024-05-21 21:09:53 -0500

    Se agregan pruebas post y page para cualquier tipo de dispositivo

--- cypress/support/constantes.js
+++ cypress/support/constantes.js
@@ -13,4 +13,26 @@ export class Constantes {
   
   //4 ó 5
   static VERSION_GHOST =5;
+  static device = 'ipad-2'; //ipad-2
 }
+
+/* 
+ipad-2	768	1024
+ipad-mini	768	1024
+iphone-3	320	480
+iphone-4	320	480
+iphone-5	320	568
+iphone-6	375	667
+iphone-6+	414	736
+iphone-7	375	667
+iphone-8	375	667
+iphone-x	375	812
+iphone-xr	414	896
+iphone-se2	375	667
+macbook-11	1366	768
+macbook-13	1280	800
+macbook-15	1440	900
+macbook-16	1536	960
+samsung-note9	414	846
+samsung-s10
+*/
